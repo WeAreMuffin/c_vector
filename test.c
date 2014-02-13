2f92bdb094381a47f60dd6b42a4f4d6dd885c443
André Aubin <andre.aubin@lambdaweb.fr>
2014-02-13 18:39:41 +0100

    Adding more tests

--- test.c
+++ test.c
@@ -44,32 +44,63 @@ void			print_car(t_car *c)
 		c->wheels, c->doors, c->color, c->max_speed);
 }
 
+void			test(char *test, int assertion)
+{
+	printf("Testing %-60s", test);
+	assertion ? printf("\e[32m✓\e[0m") : printf("\e[31mFAIL\e[0m");
+	printf("\n");
+	assert(assertion);
+}
+
 int				main(void)
 {
 	t_vector	*v;
 	t_car		*c;
+	t_car		*sec;
 
-	// create a new vector
+	// create a new vector tests
 	v = new_vector(sizeof(t_car), NULL);
-	assert(v != NULL);
+	test("Creating a new vector<t_car>", (v != NULL));
 
 	c = create_car(4, 5, 150, 0xFFFFFF);
-	assert(c != NULL);
+	sec = create_car(0, 0, 0, 0);
+	assert(c != NULL && sec != NULL);
 
-	print_car(c);
+
+	// Push tests
 	v->push(c, v);
+	test("Pushing a first t_car element", (v->count == 1));
+	assert(v->elt_size == sizeof(t_car));
 
 	c = create_car(2, 0, 100, 0xffccaa);
 	assert(c != NULL);
-
-	print_car(c);
 	v->push(c, v);
+	test("Pushing a second t_car element", (v->count == 2));
 
-	v->pop(c, v);
+	c = create_car(1, 0, 10, 0xffffaa);
+	assert(c != NULL);
+	v->push(c, v);
+	test("Pushing a 3th t_car element", (v->count == 3));
 
-	print_car(c);
-	v->pop(c, v);
+	c = create_car(8, 3, 150, 0xffffaa);
+	assert(c != NULL);
+	v->push(c, v);
+	test("Pushing a 4th t_car element", (v->count == 4));
 
-	print_car(c);
+
+	// Pop tests
+	v->pop(sec, v);
+	test("Popping the last (4th) element", (sec != NULL && sec->wheels == 8));
+	v->pop(sec, v);
+	test("Popping the last (3th) element", (sec != NULL && sec->wheels == 1));
+	v->pop(sec, v);
+	test("Popping the last (2th) element", (sec != NULL && sec->wheels == 2));
+
+	// Repush tests
+	c = create_car(50, 3, 150, 0xffffaa);
+	assert(c != NULL);
+	v->push(c, v);
+	test("Pushing a 2th t_car element with 50 wheels", (v->count == 2));
+
 	return (0);
 }
