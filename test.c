731fd4f5cfa2c72af01c21d504fae1d6f549cbbc
Alexandre Leger <aleger@student.42.fr>
2014-02-13 19:04:58 +0100

    ajout de mon test..c

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: aaubin <aaubin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/13 00:54:38 by aaubin            #+#    #+#             */
-/*   Updated: 2014/02/13 18:46:05 by aleger           ###   ########.fr       */
+/*   Updated: 2014/02/13 19:02:39 by aleger           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,35 +57,33 @@ int				main(void)
 	c = create_car(4, 5, 150, 0xFFFFFF);
 	assert(c);
 
-	print_car(c);
+	printf("size before push : %d\n", v->size(v));
 	v->push(c, v);
-	printf("%d\n", v->size(v));
+	printf("size after push 1: %d\n", v->size(v));
+	print_car(c);
 
 	c = create_car(2, 0, 100, 0xffccaa);
 	assert(c);
 
 	print_car(c);
 	v->push(c, v);
-	printf("%d\n", v->size(v));
+	printf("size after push 2: %d\n", v->size(v));
 
 	v->pop(c, v);
-
-	print_car(c);
 	v->pop(c, v);
-	printf("%d\n", v->size(v));
 
-	print_car(c);
 	printf("\n");
 	for (i = 0; i < v->size(v); ++i) {
 		c = (t_car *)v->at(v, i);
 		print_car(c);
 	}
-	printf("\n");
+	printf("\nfront car: ");
 	print_car((t_car *)v->front(v));
-	printf("\n");
+	printf("back car: ");
 	print_car((t_car *)v->back(v));
-	printf("\n%d\n", v->is_empty(v, 20));
-	printf("\n%d\n", v->is_empty(v, 2));
-	printf("\n%zu\n", v->v_capacity(v));
+	printf("\n");
+	printf("empty true: %d\n", v->is_empty(v, 20));
+	printf("empty false: %d\n", v->is_empty(v, 2));
+	printf("capacity: %zu\n", v->v_capacity(v));
 	return (0);
 }
